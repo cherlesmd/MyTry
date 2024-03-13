ad54b65135e43df96b7d6c586a36aa9cbebb7c7d
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-03-12 20:08:16 -0700

    display currrent location

--- src/main/tryUI/src/components/map/Map.js
+++ src/main/tryUI/src/components/map/Map.js
@@ -2,24 +2,25 @@ import React, { useRef, useEffect, useState } from "react";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import mapboxgl from "!mapbox-gl";
 
-mapboxgl.accessToken = "";
+mapboxgl.accessToken = ""
 
 const Map = () => {
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+    const zoom = 13;
 
-    useEffect(() => {
-        if (map.current) return; // initialize map only once
-        map.current = new mapboxgl.Map({
-            container: mapContainer.current,
-            style: "mapbox://styles/mapbox/streets-v12",
-            center: [lng, lat],
-            zoom: zoom,
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            if (map.current) return; // initialize map only once
+            map.current = new mapboxgl.Map({
+                container: mapContainer.current,
+                style: "mapbox://styles/mapbox/streets-v12",
+                center: [position.coords.longitude, position.coords.latitude],
+                zoom: zoom,
+            });
         });
-    });
+    }
+
     return <div ref={mapContainer} className="h-96 w-5/12" />;
 };
 export default Map;
