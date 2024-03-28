93b9a47616a51027c66c9d1fca74e0de5694fab7
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-03-28 14:21:38 -0700

    basic element display

--- src/main/tryUI/src/components/map/Map.js
+++ src/main/tryUI/src/components/map/Map.js
@@ -2,24 +2,34 @@ import React, { useRef, useEffect, useState } from "react";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import mapboxgl from "!mapbox-gl";
 
-mapboxgl.accessToken = ""
+mapboxgl.accessToken =
+    "";
 
 const Map = () => {
     const mapContainer = useRef(null);
     const map = useRef(null);
     const zoom = 13;
-
-    if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition((position) => {
-            if (map.current) return; // initialize map only once
-            map.current = new mapboxgl.Map({
-                container: mapContainer.current,
-                style: "mapbox://styles/mapbox/streets-v12",
-                center: [position.coords.longitude, position.coords.latitude],
-                zoom: zoom,
+    const setLng = useState(null);
+    const setLat = useState(null);
+    const setZoom = useState(null);
+    useEffect(() => {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                if (map.current) return; // initialize map only once
+                map.current = new mapboxgl.Map({
+                    container: mapContainer.current,
+                    style: "mapbox://styles/mapbox/streets-v12",
+                    center: [position.coords.longitude, position.coords.latitude],
+                    zoom: zoom,
+                });
+                map.current.on("move", () => {
+                    setLng(map.current.getCenter().lng.toFixed(4));
+                    setLat(map.current.getCenter().lat.toFixed(4));
+                    setZoom(map.current.getZoom().toFixed(2));
+                });
             });
-        });
-    }
+        }
+    });
 
     return <div ref={mapContainer} className="h-96 w-5/12" />;
 };
