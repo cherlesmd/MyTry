1edc70d34a408a1d5a3af6419fb9d4088894a182
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-07-23 14:34:04 -0700

    service autowire

--- src/main/java/com/charliemartinezdominguez/MyTry/config/JwtAuthenticationFilter.java
+++ src/main/java/com/charliemartinezdominguez/MyTry/config/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.charliemartinezdominguez.MyTry.config;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -24,6 +25,7 @@ import lombok.RequiredArgsConstructor;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final UserDetailsService userDetailsService;
+    @Autowired
     private final JwtService jwtService;
 
     @Override
