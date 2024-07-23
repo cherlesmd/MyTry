0c693d58707d74844c209ec59281e461d6826d96
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-07-23 15:07:52 -0700

    userRepo autowire

--- src/main/java/com/charliemartinezdominguez/MyTry/auth/AuthenticationController.java
+++ src/main/java/com/charliemartinezdominguez/MyTry/auth/AuthenticationController.java
@@ -2,6 +2,7 @@ package com.charliemartinezdominguez.MyTry.auth;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ import lombok.RequiredArgsConstructor;
 public class AuthenticationController {
 
     private final AuthenticationService service;
+    @Autowired
     private final JwtService jwtService;
 
     @PostMapping("/register")
