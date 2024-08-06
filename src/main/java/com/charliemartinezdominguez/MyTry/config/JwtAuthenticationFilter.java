326af0829f7611ce745058aade20adb610ea78dd
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-08-06 12:26:20 -0700

    lazy pt2

--- src/main/java/com/charliemartinezdominguez/MyTry/config/JwtAuthenticationFilter.java
+++ src/main/java/com/charliemartinezdominguez/MyTry/config/JwtAuthenticationFilter.java
@@ -3,6 +3,7 @@ package com.charliemartinezdominguez.MyTry.config;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,6 +21,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 
+@Lazy
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
