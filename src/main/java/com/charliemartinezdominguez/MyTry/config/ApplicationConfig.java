22664aaa722928897364c4dff7b7caa23f7ec493
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-08-06 12:36:05 -0700

    enable class

--- src/main/java/com/charliemartinezdominguez/MyTry/config/ApplicationConfig.java
+++ src/main/java/com/charliemartinezdominguez/MyTry/config/ApplicationConfig.java
@@ -24,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 @Lazy
 @Configuration
 @RequiredArgsConstructor
+@EnableMongoRepositories(basePackageClasses = UserRepository.class)
 public class ApplicationConfig {
 
     private final UserRepository repository;
