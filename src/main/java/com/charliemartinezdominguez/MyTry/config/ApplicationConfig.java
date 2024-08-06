a7271723e672d552f59eb1933450d33f3a3d8acd
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-08-06 13:00:46 -0700

    annotations pt3

--- src/main/java/com/charliemartinezdominguez/MyTry/config/ApplicationConfig.java
+++ src/main/java/com/charliemartinezdominguez/MyTry/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package com.charliemartinezdominguez.MyTry.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +27,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
+    @Autowired
     private final UserRepository repository;
 
     @Bean
