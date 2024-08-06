b400ee3173d075a27b8ab9f42c31fc6462cbc4b9
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-08-06 12:22:30 -0700

    lazy annotation

--- src/main/java/com/charliemartinezdominguez/MyTry/config/ApplicationConfig.java
+++ src/main/java/com/charliemartinezdominguez/MyTry/config/ApplicationConfig.java
@@ -3,6 +3,7 @@ package com.charliemartinezdominguez.MyTry.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
@@ -20,24 +21,12 @@ import com.charliemartinezdominguez.MyTry.user.UserRepository;
 
 import lombok.RequiredArgsConstructor;
 
+@Lazy
 @Configuration
 @RequiredArgsConstructor
 public class ApplicationConfig {
 
     private final UserRepository repository;
-
-    @Value("${spring.data.mongodb.uri}")
-    private String mongoUri;
-
-    @Bean
-    public MongoDatabaseFactory mongoDbFactory() {
-        return new SimpleMongoClientDatabaseFactory(mongoUri);
-    }
-
-    @Bean
-    public MongoTemplate mongoTemplate() {
-        return new MongoTemplate(mongoDbFactory());
-    }
 
     @Bean
     public UserDetailsService userDetailsService() {
