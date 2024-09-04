0b5c26672896f83a11c27422c3376a320a128f41
cherlesmd <72123666+cherlesmd@users.noreply.github.com>
2024-09-03 21:11:09 -0700

    cors

--- src/main/java/com/charliemartinezdominguez/MyTry/config/SecurityConfig.java
+++ src/main/java/com/charliemartinezdominguez/MyTry/config/SecurityConfig.java
@@ -46,8 +46,8 @@ public class SecurityConfig {
         CorsConfiguration ccfg = new CorsConfiguration();
         ccfg.setAllowedOrigins(Arrays.asList("https://mytryfrontend-490397e0f41d.herokuapp.com"));
         ccfg.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PUT", "OPTIONS", "PATCH", "DELETE"));
-        ccfg.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-        ccfg.setExposedHeaders(List.of("Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
+        ccfg.setAllowedHeaders(Arrays.asList("*"));
+        ccfg.setExposedHeaders(Arrays.asList("*"));
         ccfg.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", ccfg);
